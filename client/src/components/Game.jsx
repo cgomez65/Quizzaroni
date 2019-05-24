57132e72beb4e600db598f6905689a1c7aabb641
cgomez65 <carlosgomez7361@gmail.com>
2019-05-24 15:19:43 -0600

    added game page

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -1,41 +1,34 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
-
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementById("choice-text"))
-
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
-
-let questions
-
-
-
-
+import questions from '../../../database/data'
+console.log(questions)
 class Game extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: questions
+    }
+  }
   render(){
     return (
       <div className='container'>
         <div id='game' className='justify-center flex-column'>
-          <h2 id='question'>What is the answer to this questions?</h2>
+          <h2 id = 'question' >{ this.state.data.questions[0].question }</h2>
           <div className='choice-container'>
-            <p class='choice-prefix'>A</p>
-            <p class='choice-text' data-number='1'>Choice 1</p>
+            <p className='choice-prefix'>A</p>
+            <p className='choice-text' data-number='1'>{ this.state.data.questions[0].choice1 }</p>
           </div>
           <div className='choice-container'>
-            <p class='choice-prefix'>B</p>
-            <p class='choice-text' data-number='2'>Choice 2</p>
+            <p className='choice-prefix'>B</p>
+            <p className='choice-text' data-number='2'>{ this.state.data.questions[0].choice2 }</p>
           </div>
           <div className='choice-container'>
-            <p class='choice-prefix'>C</p>
-            <p class='choice-text' data-number='3'>Choice 3</p>
+            <p className='choice-prefix'>C</p>
+            <p className='choice-text' data-number='3'>{ this.state.data.questions[0].choice3 }</p>
           </div>
           <div className='choice-container'>
-            <p class='choice-prefix'>D</p>
-            <p class='choice-text' data-number='4'>Choice 4</p>
+            <p className='choice-prefix'>D</p>
+            <p className='choice-text' data-number='4'>{ this.state.data.questions[0].choice4 }</p>
           </div>
         </div>
       </div>
